4c830b8ad0f0776370761c6f88f9e9c821dbb6b3
George Ornbo <george@shapeshed.com>
2024-11-06 11:10:46 +0000

    Initial implementation

--- cmd/gasstation/main.go
+++ cmd/gasstation/main.go
@@ -6,21 +6,27 @@ import (
 	"os"
 
 	"github.com/BurntSushi/toml"
-	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/shapeshed/gasstation/internal/grpc"
 	"github.com/shapeshed/gasstation/internal/queries"
+
+	sdkmath "cosmossdk.io/math"
+
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 )
 
 // ChainConfig represents the configuration for each chain in the TOML file.
-type ChainConfig struct {
-	Name     string   `toml:"name"`
-	GRPCURL  string   `toml:"grpc_url"`
-	Accounts []string `toml:"accounts"`
+type Chain struct {
+	Name         string   `toml:"name"`
+	GRPCURL      string   `toml:"grpc_url"`
+	GasDenom     string   `toml:"gas_denom"`
+	Accounts     []string `toml:"accounts"`
+	Threshold    int64    `toml:"threshold"`
+	AmountToFund int64    `toml:"amount_to_fund"`
 }
 
 // Config represents the entire configuration file.
 type Config struct {
-	Chains []ChainConfig `toml:"chains"`
+	Chains []Chain `toml:"chains"`
 }
 
 func loadConfig(path string) (*Config, error) {
@@ -33,7 +39,7 @@ func loadConfig(path string) (*Config, error) {
 
 func main() {
 	// Load configuration from TOML
-	config, err := loadConfig("config.toml")
+	config, err := loadConfig("configs/config.toml")
 	if err != nil {
 		slog.Error("Failed to load configuration", "error", err)
 		os.Exit(1)
@@ -61,10 +67,15 @@ func main() {
 
 		// Check balance for each account in the chain
 		for _, account := range chain.Accounts {
-			balance, err := queries.GetBalance(ctx, bankQueryClient, account, "untrn")
+			balance, err := queries.GetBalance(ctx, bankQueryClient, account, chain.GasDenom)
 			if err != nil {
 				slog.Error("Error retrieving balance", "chain", chain.Name, "account", account, "error", err)
 				continue
+			}
+			threshold := sdkmath.NewInt(chain.Threshold)
+
+			if balance.Balance.Amount.LT(threshold) {
+				slog.Info("Balance is less than threshold, should fund")
 			}
 
 			slog.Info("Balance checked", "chain", chain.Name, "account", account, "balance", balance)
